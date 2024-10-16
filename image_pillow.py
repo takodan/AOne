ec578d30b33c3571805289f250bc173b587356f0
takodan <takodan24@gmail.com>
2024-10-16 01:45:02 -0700

    add cloudflare_worker_ai.py

--- image_pillow.py
+++ image_pillow.py
@@ -19,23 +19,36 @@ def heic_to_png(image_path):
     cv2.imwrite(f"receipt_image\{png_name}", np_array)
 
     with Image.open(f"receipt_image\{png_name}") as png_im:
-        png_im.thumbnail((1600, 1600))
+        png_im.thumbnail((320, 320))
         png_im.save(f"receipt_image\{png_name}", 'PNG', optimize=True )
-
 
 
 def png_to_webp(image_path):
     png_name = os.path.basename(image_path)
     webp_name = png_name.replace("png", "webp")
-    with Image.open("hopper.jpg") as im:
+    with Image.open(image_path) as im:
         im.save(f"{webp_name}", 'WebP', quality=60, )
+
+def png_to_jpeg(image_path):
+    png_name = os.path.basename(image_path)
+    jpeg_name = png_name.replace("png", "jpg")
+    with Image.open(image_path) as im:
+        im.save(f"{jpeg_name}", 'JPEG')
         
 
+def png_to_png_gray_scale(image_path):
+    png_name = os.path.basename(image_path)
+    gray_name = png_name.replace(".png", "_gray.png")
+    with Image.open(image_path) as im:
+        im = im.convert('L')
+        im.save(f"{gray_name}", 'PNG')
 
 
 
 if __name__ == "__main__":
     heic_to_png(r"C:\danny\2_code\2_repo\AOne\receipt_image\IMG_0641.HEIC")
+    # png_to_webp(r"C:\danny\2_code\2_repo\AOne\receipt_image\IMG_0641.png")
+    # png_to_jpeg(r"C:\danny\2_code\2_repo\AOne\receipt_image\IMG_0641.png")
 
     # input_image_path = input("Enter the image path: ")
     # fun = input("Enter a number to select function:\n\
--- worker_ai.py
+++ worker_ai.py
@@ -1,5 +1,4 @@
 # Cloudflare Workers AI REST API
-import os
 import requests
 import hidden
 import base64
@@ -32,35 +31,28 @@ def read_image_binary(image_path):
     
 def read_image_array(image_path):
     with Image.open(image_path) as image_file:
-        image = image_file.convert('RGB')
+        image = image_file.convert('L')
         image_array = np.array(image)
-        image_flat_array = image_array.flatten().tolist()
-        return image_flat_array
+        image_array_flat = image_array.flatten().tolist()
+        return image_array_flat
 
 def read_image_tensor(image_path):
     with Image.open(image_path) as image_file:
         image = image_file.convert('L')
-        transform = transforms.Compose([
-            transforms.PILToTensor()
-        ])
+        transform = transforms.Compose([transforms.PILToTensor()])
         img_tensor = transform(image)
-        flattened_array = img_tensor.flatten().tolist()
-        return flattened_array
+        array_tensor = img_tensor.flatten().tolist()
+        return array_tensor
 
-def receipt_ocr(model, image_data):
+
+def cat_prompt_only(model, image_data):
     print(type(image_data))
     # post
     response = requests.post(
         f"{API_BASE_URL}{model}",
         headers = {"Authorization": f"Bearer {AUTH_TOKEN}"},
         json = {
-            "messages": [
-                {"role": "system", "content": "You output a JSON object that includes receipt details such as store name, date, purchased items."},
-                {"role": "user", "content": "Give me the store name"},
-                {"role": "assistant", "content": "{\"store_name\":\"Trader Joe\'s\"}"},
-                {"role": "user", "content": "Give me the store name from this picture"},
-                # {"role": "assistant", "content": ""}
-            ],
+            "prompt": "What's the animal in the picture?",
             "image": image_data,
         }
     )
@@ -69,28 +61,10 @@ def receipt_ocr(model, image_data):
         result = response.json()
         return result
     else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return  
-
-
-def receipt_ocr_prompt_only(model, image_data):
-    print(type(image_data))
-    # post
-    response = requests.post(
-        f"{API_BASE_URL}{model}",
-        headers = {"Authorization": f"Bearer {AUTH_TOKEN}"},
-        json = {
-            "prompt": "Give me the store name from this picture",
-            "image": image_data,
-        }
-    )
-
-    if response.status_code == 200:
-        result = response.json()
-        return result
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return  
+        print(f"Error: {response.status_code}, Message: {response.text}")
+        with open("output.txt", "a") as f:
+            f.write(response.text)
+        return "Error!"
     
 
 if __name__ == "__main__":
@@ -107,6 +81,7 @@ if __name__ == "__main__":
     # im = read_image_tensor(r"cat.png")
 
     # orc receipt
-    output = receipt_ocr("@cf/meta/llama-3.2-11b-vision-instruct", im)
+    output = cat_prompt_only("@cf/meta/llama-3.2-11b-vision-instruct", im)
     # output = receipt_ocr_prompt_only("@cf/meta/llama-3.2-11b-vision-instruct", im)
+
     print(output)
